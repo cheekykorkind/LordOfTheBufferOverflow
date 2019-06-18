d93c3d14ca11b62a55c1d1bdb389a84d729adb5c
cheekyorkind <cheekorkind@gmail.com>
2019-06-19 00:20:47 +0900

    first overflow

---
+++ quiz1/real_quiz_address_cal.c
@@ -0,0 +1,14 @@
+// gcc -o real_quiz_address_cal real_quiz_address_cal.c -static
+#define NULL 0
+
+int main()
+{
+	int auth = 0;
+    char passwd[20];
+    int *authPtr = &auth;
+    char *passwdPtr = &passwd;
+
+    printf("auth memory address : 0x%x\n", authPtr);
+    printf("passwd memory address : 0x%x\n", passwdPtr);
+    printf("sub address : %d\n", (int) authPtr - (int) passwdPtr);
+}
