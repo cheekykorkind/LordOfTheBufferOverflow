5557735595ab0d69ad60baeb67da0323c2473afd
cheekyorkind <cheekorkind@gmail.com>
2019-06-20 00:25:59 +0900

    Little Endian

---
+++ 11/ex2.c
@@ -0,0 +1,18 @@
+// gcc -o ex2 ex2.c -static
+
+int main()
+{
+    int i;
+    int integer = 10;
+    unsigned char *integerAddress = (unsigned char *)&integer;
+
+    printf("===============HEX DUMP START===============\n");
+
+    printf("0x%08x  ", &integerAddress);
+
+    for (i = 0; i < sizeof(integer); i++) {
+        printf("0x%02x ", *(integerAddress + i) );
+    }
+
+    printf("\n===============HEX DUMP END===============\n");
+}
